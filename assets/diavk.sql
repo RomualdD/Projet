dabf4f8b6256aa9b9e820660c61c78801a61ea38
RomualdD <romualdducrocq1@gmail.com>
2018-02-06 09:00:22 +0100

    Modification sql

--- assets/diavk.sql
+++ assets/diavk.sql
@@ -1,54 +1,57 @@
-CREATE DATABASE diavk;
+CREATE DATABASE `diavk`;
 
-USE diavk;
+USE `diavk`;
 
-CREATE TABLE utilisateurs (
-	id INT(11) AUTO_INCREMENT PRIMARY KEY,
-	nom VARCHAR(40),
-	prenom VARCHAR(60),
-	nom_utilisateur VARCHAR(40),
-	mail VARCHAR(80),
-	mot_de_passe VARCHAR(80),
-	date_anniversaire DATE,
-	phone VARCHAR(12),
-	phone2 VARCHAR(12),
-	role INT(11),
-	pathologie INT(11),
-	cleverif varchar(32),
-	actif INT(11)	
+CREATE TABLE `users` (
+	`id` INT(11) AUTO_INCREMENT PRIMARY KEY,
+	`lastname` VARCHAR(40),
+	`firstname` VARCHAR(60),
+	`username` VARCHAR(40),
+	`mail` VARCHAR(80),
+	`password` VARCHAR(80),
+	`birthdate` DATE,
+	`phone` VARCHAR(12),
+	`phone2` VARCHAR(12),
+	`role` INT(11),
+	`pathology` INT(11),
+	`keyverif` varchar(32),
+	`active` INT(11),
+	`qrcode` VARCHAR(60)	
 );
 
-CREATE TABLE suivis (
-	id INT(11) PRIMARY KEY,
-	id_utilisateur INT(11),
-	date_du_jour DATETIME,
-	resultat VARCHAR(10),
-	date_prochaine_verif DATETIME
+CREATE TABLE `medical_followup` (
+	`id` INT(11) PRIMARY KEY,
+	`userId` INT(11),
+	`today_date` DATETIME,
+	`result` VARCHAR(10),
+	`next_date_check` DATETIME
 );
 
-CREATE TABLE follow (
-	follow_from INT(11),
-	follow_to INT(11),
-	follow_confirm enum('0','1') NOT NULL,
-	follow_date DATE,
-	CONSTRAINT PK_Follow PRIMARY KEY (follow_from, follow_to)
+CREATE TABLE `follow` (
+	`follow_from` INT(11),
+	`follow_to` INT(11),
+	`follow_confirm` enum('0','1') NOT NULL,
+	`follow_date` DATE,
+	CONSTRAINT PK_Follow PRIMARY KEY (`follow_from`, `follow_to`)
 );
 
-CREATE TABLE verification ( 
-	id_utilisateur INT(11) PRIMARY KEY,
-	verification DATETIME,
-	Heure1 VARCHAR(40),
-	Heure2 VARCHAR(40),
-	Heure3 VARCHAR(40),
-	Heure4 VARCHAR(40),
-	notification INT(11)
+CREATE TABLE `verification` ( 
+	`userId` INT(11) PRIMARY KEY,
+	`verification` DATETIME,
+	`onehour` VARCHAR(40),
+	`twohour` VARCHAR(40),
+	`threehour` VARCHAR(40),
+	`fourhour` VARCHAR(40),
+	`notification` INT(11),
+	`verirication_date` VARCHAR(40)
 );
 
-CREATE TABLE rendez_vous ( 
-	id INT(11) PRIMARY KEY AUTO_INCREMENT,
-	id_utilisateur INT(12),
-	nom_rendez_vous VARCHAR(255),
-	date_rendez_vous DATE,
-	heure_rendez_vous VARCHAR(40),
-	infos_complementaire VARCHAR(255)
+CREATE TABLE `appointments` ( 
+	`id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+	`userId` INT(12),
+	`name_appointment` VARCHAR(255),
+	`date_appointment` DATE,
+	`hour_appointment` VARCHAR(40),
+	`additional_informations` VARCHAR(255),
+	`remarque` VARCHAR(255)
 );
