57abfd9fe7ee5cb5733a492bc5aee78be1ea4127
RomualdD <romualdducrocq1@gmail.com>
2018-03-20 16:02:04 +0100

    Création des tables pathology et role + Modification MCD

--- assets/newsql.sql
+++ assets/newsql.sql
@@ -1,12 +1,20 @@
+
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
 CREATE DATABASE `diavk`;
 
 USE `diavk`;
 
+#------------------------------------------------------------
+# Table: pbvhfjt_appointments
+#------------------------------------------------------------
+
 CREATE TABLE pbvhfjt_appointments(
         id                      int (11) Auto_increment  NOT NULL ,
-        name_appointment        Varchar (255) ,
-        date_appointment        Date ,
-        hour_appointment        Varchar (40) ,
+        name                    Varchar (255) ,
+        date                    Date ,
+        hour                    Varchar (40) ,
         additional_informations Varchar (255) ,
         remarque                Varchar (255) ,
         id_pbvhfjt_users        Int NOT NULL ,
@@ -20,8 +28,8 @@ CREATE TABLE pbvhfjt_appointments(
 
 CREATE TABLE pbvhfjt_follow(
         id                 int (11) Auto_increment  NOT NULL ,
-        follow_confirm     Enum ("0","1") NOT NULL ,
-        follow_date        Date ,
+        confirm            Enum ("0","1") NOT NULL ,
+        date               Date ,
         id_pbvhfjt_users   Int NOT NULL ,
         id_pbvhfjt_users_1 Int NOT NULL ,
         PRIMARY KEY (id )
@@ -47,21 +55,21 @@ CREATE TABLE pbvhfjt_medical_followup(
 #------------------------------------------------------------
 
 CREATE TABLE pbvhfjt_users(
-        id        int (11) Auto_increment  NOT NULL ,
-        lastname  Varchar (40) ,
-        firstname Varchar (60) ,
-        username  Varchar (40) ,
-        mail      Varchar (80) ,
-        password  Varchar (80) ,
-        birthdate Date ,
-        phone     Varchar (12) ,
-        phone2    Varchar (12) ,
-        role      Int ,
-        pathology Int ,
-	language  Char (5),
-        keyverif  Varchar (32) ,
-        active    Int ,
-        qrcode    Varchar (60) ,
+        id                   int (11) Auto_increment  NOT NULL ,
+        lastname             Varchar (40) ,
+        firstname            Varchar (60) ,
+        username             Varchar (40) ,
+        mail                 Varchar (80) ,
+        password             Varchar (80) ,
+        birthdate            Date ,
+        phone                Varchar (12) ,
+        phone2               Varchar (12) ,
+        language             Char (5) ,
+        keyverif             Varchar (32) ,
+        active               Int ,
+        qrcode               Varchar (60) ,
+        id_pbvhfjt_role      Int NOT NULL ,
+        id_pbvhfjt_pathology Int NOT NULL ,
         PRIMARY KEY (id )
 )ENGINE=InnoDB;
 
@@ -73,13 +81,35 @@ CREATE TABLE pbvhfjt_users(
 CREATE TABLE pbvhfjt_verification(
         id                int (11) Auto_increment  NOT NULL ,
         verification      Varchar (12) ,
-        onehour           Varchar (40) ,
-        twohour           Varchar (40) ,
-        threehour         Varchar (40) ,
-        fourhour          Varchar (40) ,
+        one_hour          Varchar (40) ,
+        two_hour          Varchar (40) ,
+        three_hour        Varchar (40) ,
+        four_hour         Varchar (40) ,
         notification      Int ,
         verification_date Varchar (40) ,
         id_pbvhfjt_users  Int NOT NULL ,
+        PRIMARY KEY (id )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: pbvhfjt_pathology
+#------------------------------------------------------------
+
+CREATE TABLE pbvhfjt_pathology(
+        id   int (11) Auto_increment  NOT NULL ,
+        name Varchar (255) NOT NULL ,
+        PRIMARY KEY (id )
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: pbvhfjt_role
+#------------------------------------------------------------
+
+CREATE TABLE pbvhfjt_role(
+        id   int (11) Auto_increment  NOT NULL ,
+        name Varchar (255) ,
         PRIMARY KEY (id )
 )ENGINE=InnoDB;
 
@@ -87,4 +117,7 @@ ALTER TABLE pbvhfjt_appointments ADD CONSTRAINT FK_pbvhfjt_appointments_id_pbvhf
 ALTER TABLE pbvhfjt_follow ADD CONSTRAINT FK_pbvhfjt_follow_id_pbvhfjt_users FOREIGN KEY (id_pbvhfjt_users) REFERENCES pbvhfjt_users(id);
 ALTER TABLE pbvhfjt_follow ADD CONSTRAINT FK_pbvhfjt_follow_id_pbvhfjt_users_1 FOREIGN KEY (id_pbvhfjt_users_1) REFERENCES pbvhfjt_users(id);
 ALTER TABLE pbvhfjt_medical_followup ADD CONSTRAINT FK_pbvhfjt_medical_followup_id_pbvhfjt_users FOREIGN KEY (id_pbvhfjt_users) REFERENCES pbvhfjt_users(id);
+ALTER TABLE pbvhfjt_users ADD CONSTRAINT FK_pbvhfjt_users_id_pbvhfjt_role FOREIGN KEY (id_pbvhfjt_role) REFERENCES pbvhfjt_role(id);
+ALTER TABLE pbvhfjt_users ADD CONSTRAINT FK_pbvhfjt_users_id_pbvhfjt_pathology FOREIGN KEY (id_pbvhfjt_pathology) REFERENCES pbvhfjt_pathology(id);
 ALTER TABLE pbvhfjt_verification ADD CONSTRAINT FK_pbvhfjt_verification_id_pbvhfjt_users FOREIGN KEY (id_pbvhfjt_users) REFERENCES pbvhfjt_users(id);
+
